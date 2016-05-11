3bd27b53598ed07bcbc9d6dc67c59920e920f3e7
Vadym Bartko <vadymbartko@ITs-Mac-mini.local>
2016-05-11 15:43:28 +0300

    fixes

--- src/main/java/com/hudvin/hadoop/playground/HadoopUtils.java
+++ src/main/java/com/hudvin/hadoop/playground/HadoopUtils.java
@@ -1,8 +1,21 @@
 package com.hudvin.hadoop.playground;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
 /**
  * Created by kontiki on 10.05.16.
  */
 public class HadoopUtils {
+
+    public static void removeOutputDir(Path outputDir, Configuration conf) throws IOException {
+        FileSystem fs = outputDir.getFileSystem(conf);
+        if (outputDir.toString().contains("/tmp") && fs.exists(outputDir)){
+            fs.delete(outputDir, true);
+        }
+    }
 
 }
